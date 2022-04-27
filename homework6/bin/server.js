243603fc72a2019d7c5c145600fa2890b248e3c3
workdmitriy.t@gmail.com <gdhetebs7j>
2022-04-27 18:08:46 +0300

    change default port

--- homework6/bin/server.js
+++ homework6/bin/server.js
@@ -2,14 +2,14 @@
   const mongoose = require('mongoose')
   require('dotenv').config()
   const app = require('../app')
-  const { PORT = 3000, DB_HOST } = process.env
+  const { PORT = 4000, DB_HOST } = process.env
   try {
     await mongoose.connect(DB_HOST, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
     app.listen(PORT)
-    console.log('Database connection successful')
+    console.log(`Database connection successful and list on port ${PORT}`)
   } catch (e) {
     console.log(e.message)
     process.exit(1)
